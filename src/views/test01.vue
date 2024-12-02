31070492abe757f994f9ad7a956cfc565aea2128
sugiuraso <sugiuraso@yamaha-motor.co.jp>
2024-12-02 14:01:15 +0900

    [AL00594(Soma Sugiura) on 20241202050114]Hello World追加

---
+++ src/views/test01.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useLockScreen } from 'viy-ui';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+const { lockScreen } = useLockScreen();
+
+defineOptions({
+  name: 'test01',
+});
+
+const form = ref();
+
+const formData = reactive({
+});
+</script>
+
+<template>
+  <VueForm ref="form" v-loading="lockScreen" :model="formData">
+    <VueText id="viy2Text_7OwS0" ref="viy2Text_7OwS0" :style="{ width: '100%', display: 'inline-block' }">
+      Hello World
+    </VueText>
+  </VueForm>
+</template>
